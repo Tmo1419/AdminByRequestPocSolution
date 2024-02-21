bc5dbda2c7dbf2e9c302ad1ae1797f0b0ae8405d
Torben Mortensen <tm@business-objects.dk>
2024-02-21 22:33:13 +0100

    Updated various.

--- ProductCatalogService/Program.cs
+++ ProductCatalogService/Program.cs
@@ -2,8 +2,6 @@ using Microsoft.EntityFrameworkCore;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using AspNetCoreRateLimit;
-using MongoDB.Driver;
-using MongoDB.Bson;
 using ProductCatalogService.Infrastructure.Data;
 using ProductCatalogService.Core.Domain.Entities;
 using Asp.Versioning;
@@ -78,55 +76,6 @@ app.UseHttpsRedirection();
 app.UseRouting();
 app.MapControllers();
 
-// Next is Mongo.
-const string connectionUri = "mongodb+srv://tmproduct:F17n6fppU1iGCaSy@cluster0.asw8qts.mongodb.net/?retryWrites=true&w=majority";
-var settings = MongoClientSettings.FromConnectionString(connectionUri);
-
-// Set the ServerApi field of the settings object to set the version of the Stable API on the client
-settings.ServerApi = new ServerApi(ServerApiVersion.V1);
-// Create a new client and connect to the server
-var client = new MongoClient(settings);
-// Send a ping to confirm a successful connection
-try
-{
-    var result = client.GetDatabase("admin").RunCommand<BsonDocument>(new BsonDocument("ping", 1));
-    Console.WriteLine("Pinged your deployment. You successfully connected to MongoDB!");
-
-    var playlistCollection = client.GetDatabase("sample_mflix").GetCollection<Product2>("product2");
-
-    List<string> movieList = ["1234"];
-
-    playlistCollection.InsertOne(new Product2("nraboy", movieList));
-
-    FilterDefinition<Product2> filter = Builders<Product2>.Filter.Eq("Title", "nraboy");
-
-
-    UpdateDefinition<Product2> update = Builders<Product2>.Update.AddToSet<string>("Features", "5678");
-
-    playlistCollection.UpdateOne(filter, update);
-
-    List<Product2> results = playlistCollection.Find(filter).ToList();
-
-
-    String names = "";
-    foreach (Product2 res in results)
-    {
-        names = names + res.Features;
-        Console.WriteLine(string.Join(", ", res.Features));
-    }
-
-    List<string> databases = client.ListDatabaseNames().ToList();
-
-    foreach (string database in databases)
-    {
-        Console.WriteLine(database);
-    }
-}
-catch (Exception ex)
-{
-    Console.WriteLine(ex);
-}
-// Mongo End.
 
 
 app.Run();
